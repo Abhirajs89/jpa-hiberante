053c646f51e3b4ed96f0c7097735976349264212
Abhiraj Suryawanshi <suryawanshi.abhiraj1@gmail.com>
2022-09-28 14:04:16 -0300

    Added code for Spring data JPA CRUD operation

--- jpa-and-hibernate/src/main/java/com/abhiraj/springboot/jpaandhibernate/course/CourseCommandLineRunner.java
+++ jpa-and-hibernate/src/main/java/com/abhiraj/springboot/jpaandhibernate/course/CourseCommandLineRunner.java
@@ -2,6 +2,7 @@ package com.abhiraj.springboot.jpaandhibernate.course;
 
 import com.abhiraj.springboot.jpaandhibernate.course.jdbc.CourseJdbcRepository;
 import com.abhiraj.springboot.jpaandhibernate.course.jpa.CourseJpaRepository;
+import com.abhiraj.springboot.jpaandhibernate.course.springdatajpa.CourseSpringDataJpaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,13 +13,16 @@ public class CourseCommandLineRunner implements CommandLineRunner {
     //@Autowired
    // private CourseJdbcRepository repository;
 
+    //@Autowired
+    //private CourseJpaRepository repository;
+
     @Autowired
-    private CourseJpaRepository repository;
+    private CourseSpringDataJpaRepository repository;
 
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert(new Course(1,"Learn Spring JDBC","Abhiraj"));
+       /* repository.insert(new Course(1,"Learn Spring JDBC","Abhiraj"));
         repository.insert(new Course(2,"Learn AWS JPA","Anuja"));
         repository.insert(new Course(3,"Learn JPA Test","Vivek"));
         repository.insert(new Course(4,"Learn Java","Sayali"));
@@ -26,6 +30,22 @@ public class CourseCommandLineRunner implements CommandLineRunner {
         repository.delete(4);
 
         System.out.println(repository.findById(2));
-        System.out.println(repository.findById(3));
+        System.out.println(repository.findById(3));*/
+
+        repository.save(new Course(1,"Learn Spring JDBC","Abhiraj"));
+        repository.save(new Course(2,"Learn AWS Spring Data JPA","Anuja"));
+        repository.save(new Course(3,"Learn Spring Data JPA","Vivek"));
+        repository.save(new Course(4,"Learn Java","Sayali"));
+
+        repository.deleteById(4l);
+
+        System.out.println(repository.findById(2l));
+        System.out.println(repository.findById(3l));
+
+        System.out.println(repository.findAll());
+        System.out.println(repository.count());
+
+        System.out.println(repository.findByAuthor("Abhiraj"));
+        System.out.println(repository.findByName("Learn Spring Data JPA"));
     }
 }
