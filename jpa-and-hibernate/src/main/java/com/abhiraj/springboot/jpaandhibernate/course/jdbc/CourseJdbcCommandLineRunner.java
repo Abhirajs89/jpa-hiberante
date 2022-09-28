10d65da82715b35e0b568c2913d937f78cd60ba6
Abhiraj Suryawanshi <suryawanshi.abhiraj1@gmail.com>
2022-09-27 21:16:34 -0300

    Added code for spring JDBC insert, delete operation for course

--- jpa-and-hibernate/src/main/java/com/abhiraj/springboot/jpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ jpa-and-hibernate/src/main/java/com/abhiraj/springboot/jpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.abhiraj.springboot.jpaandhibernate.course.jdbc;
 
+import com.abhiraj.springboot.jpaandhibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,11 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(1,"Learn Spring JDBC","Abhiraj"));
+        repository.insert(new Course(2,"Learn AWS","Anuja"));
+        repository.insert(new Course(3,"Learn JPA","Vivek"));
+        repository.insert(new Course(4,"Learn Java","Sayali"));
+
+        repository.delete(4);
     }
 }
